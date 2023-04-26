0964e409dbe248e192ce92c81401701cd9161fba
Ezra Kirsh <ezkirsh@gmail.com>
2023-04-26 15:10:24 -0400

    first commit 11

--- src/App.js
+++ src/App.js
@@ -11,16 +11,12 @@ function App() {
     const [result, setResult] = useState(null);
     const [collaborators, setCollaborators] = useState([]);
     const [clicked, setClicked] = useState(false);
+    const [search, setSearch] = useState(true);
     const statusRef = useRef(null);
     const [menuOpen, setMenuOpen] = useState(false);
     const [active_scrapers, setActiveScrapers] = useState([]);
     const [searchError, setSearchError] = useState(false);
 
-    function getFirstThreeSentences(text) {
-        const sentences = text.split('.');
-        const firstThreeSentences = sentences.slice(0, 3);
-        return firstThreeSentences.join('. ') + '.';
-    }
 
     useEffect(() => {
         fetch('https://fastapi-production-3513.up.railway.app/get_active_scrapers')
@@ -55,11 +51,12 @@ function App() {
                 const response = await fetch(`https://fastapi-production-3513.up.railway.app/artist-data/${artistID}`);
                 const data = await response.json();
                 console.log(data);
-                console.log(loading)
+                console.log(loading);
                 if (Array.isArray(data)) {
                     setCollaborators(data);
                     setLoading(false);
-                    console.log(loading)
+                    setSearch(false);
+                    console.log(loading);
                 }
                 else {
                     if (data['message'] === 'Scraper had an error') {
@@ -130,70 +127,76 @@ function App() {
                             </div>
                         </div>
                     )}
-                <main className="py-12">
-                <div className="container mx-auto px-8 py-12 rounded-lg shadow-lg bg-white">
-                <h2 className="text-4xl font-bold text-gray-900 mb-8">Find your next favorite writer/producer</h2>
-                <form onSubmit={handleSubmit} className="w-full max-w-xl">
-                <div className="flex items-center mb-4">
-                    <input
-                        className="rounded-l-lg px-4 py-3 bg-white text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full"
-                        type="text"
-                        placeholder="Search for an artist..."
-                        value={input}
-                        onChange={(event) => setInput(event.target.value)}
-                    />
-                    <button
-                        className="bg-slate-800 text-white rounded-r-lg px-4 py-3 transition-all duration-200 hover:bg-blue-600 focus:outline-none"
-                        type="submit"
-                    >
-                        Search
-                    </button>
-                </div>
-                </form>
-                </div>
-                </main>
-                </div>
-            )}
+                    {search ? (
+                        <main className="py-12">
+                            <div className="container mx-auto px-8 py-12 rounded-lg shadow-lg bg-white">
+                                <h2 className="text-4xl font-bold text-gray-900 mb-8">Find your next favorite writer/producer</h2>
+                                <form onSubmit={handleSubmit} className="w-full max-w-xl">
+                                    <div className="flex items-center mb-4">
+                                        <input
+                                            className="rounded-l-lg px-4 py-3 bg-white text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full"
+                                            type="text"
+                                            placeholder="Search for an artist..."
+                                            value={input}
+                                            onChange={(event) => setInput(event.target.value)}
+                                        />
+                                        <button
+                                            className="bg-slate-800 text-white rounded-r-lg px-4 py-3 transition-all duration-200 hover:bg-blue-600 focus:outline-none"
+                                            type="submit"
+                                        >
+                                            Search
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </main>
+
+                    ) : (
 
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                {collaborators.map((result, index) => (
-                    <div className="bg-white rounded-lg shadow-lg overflow-hidden mx-4 my-2 md:mx-2 md:my-4 max-w-sm">
-                        <div className="relative">
-                            <img className="w-full h-64 object-cover" src={result.info.image}
-                                 alt="{{ producer.name }}" />
-                                <div
-                                    className="absolute bottom-0 left-0 right-0 px-6 py-4 bg-gradient-to-t from-black to-transparent">
-                                    <h2 className="text-white font-bold text-xl mb-2">{result.name}</h2>
-                                    <p className="text-gray-400 text-base mb-4">{getFirstThreeSentences(result.info.description)}</p>
-                                </div>
-                        </div>
-                        <div className="px-6 py-4">
-                            <h3 className="text-gray-800 font-semibold text-lg mb-2">Frequent Collaborators:</h3>
-                            <ul className="flex flex-wrap mb-4">
-                                {result.collaborators.slice(0, 6).map((c, index) => (
-                                    <li className="text-gray-600 font-medium text-base mr-2 mb-2"><a
-                                        href="">{c.name}</a></li>
-                                ))}
-                                </ul>
-                                    <h3 className="text-gray-800 font-semibold text-lg mb-2">Top Songs:</h3>
-                                    <div className="grid grid-cols-2 gap-2">
-                                        {result.songs.slice(0,6).map((song, index) => (
-                                            <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-                                                <img className="w-full h-40 object-cover" src={song.song_art}
-                                                     alt="img" />
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                            {collaborators.map((result, index) => (
+                                <div className="bg-white rounded-lg shadow-lg overflow-hidden mx-4 my-2 md:mx-2 md:my-4 max-w-sm">
+                                    <div className="relative">
+                                        <img className="w-full h-64 object-cover" src={result.info.image}
+                                             alt="{{ producer.name }}" />
+                                        <div
+                                            className="absolute bottom-0 left-0 right-0 px-6 py-4 bg-gradient-to-t from-black to-transparent">
+                                            <h2 className="text-white font-bold text-xl mb-2">{result.name}</h2>
+                                        </div>
+                                    </div>
+                                    <div className="px-6 py-4">
+                                        <h3 className="text-gray-800 font-semibold text-lg mb-2">Frequent Collaborators:</h3>
+                                        <ul className="flex flex-wrap mb-4">
+                                            {result.collaborators.slice(0, 6).map((c, index) => (
+                                                <li className="text-gray-600 font-medium text-base mr-2 mb-2"><a
+                                                    href="">{c.name}</a></li>
+                                            ))}
+                                        </ul>
+                                        <h3 className="text-gray-800 font-semibold text-lg mb-2">Top Songs:</h3>
+                                        <div className="grid grid-cols-2 gap-2">
+                                            {result.songs.slice(0,6).map((song, index) => (
+                                                <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+                                                    <img className="w-full h-40 object-cover" src={song.song_art}
+                                                         alt="img" />
                                                     <div className="px-4 py-2">
                                                         <p className="text-gray-800 font-medium text-base">{song.title}</p>
                                                         <p className="text-gray-600 text-base">{song.primary_artist.name}</p>
                                                     </div>
-                                            </div>
-                                        ))}
+                                                </div>
+                                            ))}
+                                        </div>
+                                        <p className="text-gray-400 text-base mb-4">{result.info.description}</p>
                                     </div>
-                                    </div>
-                                    </div>
-                ))}
-            </div>
+                                </div>
+                            ))}
+                        </div>
+                        )}
+                </div>
+
+            )}
         </div>
+
 
     );
 
